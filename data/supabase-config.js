54a21c18d0be253fb75f1b47bbd43344258f80f4
ceballos007 <ceballosg007@gmail.com>
2026-08-05 23:24:12 +0200

    Create supabase-config.js
    v0.5.0 - configuracion Supabase

---
+++ data/supabase-config.js
@@ -0,0 +1,9 @@
+// =========================================================
+// MUNDO INFINITO · SUPABASE
+// =========================================================
+
+const SUPABASE_URL =
+  "https://lsuplydxuaskyrkbubee.supabase.co";
+
+const SUPABASE_KEY =
+  "sb_publishable_ywKK2SlB4w8CFSWAyJ0Xhg_f1qP6Vk9";
